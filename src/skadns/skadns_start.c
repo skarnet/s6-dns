05206843060c87c3b5c432f3fbb066ed3a5f9112
Laurent Bercot <ska-skaware@skarnet.org>
2015-01-06 00:41:36 +0000

    - bugfix in skadns, need to align to skalibs-2.1.0.0  - version increase to 2.0.0.1

--- src/skadns/skadns_start.c
+++ src/skadns/skadns_start.c
@@ -6,5 +6,5 @@
 
 int skadns_start (skadns_t *a, char const *path, tain_t const *deadline, tain_t *stamp)
 {
-  return skaclient_start_b(&a->connection, &a->buffers, path, SKADNS_BANNER1, SKADNS_BANNER1_LEN, SKADNS_BANNER2, SKADNS_BANNER2_LEN, deadline, stamp) ;
+  return skaclient_start_b(&a->connection, &a->buffers, path, 0, SKADNS_BANNER1, SKADNS_BANNER1_LEN, SKADNS_BANNER2, SKADNS_BANNER2_LEN, deadline, stamp) ;
 }
