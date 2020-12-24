c6e30fe7be92b1fcbf50d764e3e5900ae5445984
Laurent Bercot <ska-skaware@skarnet.org>
2020-12-24 00:46:31 +0000

    s6dns_message_parse_question() needs to update counts

--- src/libs6dns/s6dns_message_parse_question.c
+++ src/libs6dns/s6dns_message_parse_question.c
@@ -8,13 +8,15 @@
 #include <s6-dns/s6dns-constants.h>
 #include <s6-dns/s6dns-message.h>
 
-int s6dns_message_parse_question (s6dns_domain_t *name, uint16_t *qtype, char const *packet, unsigned int packetlen, unsigned int *pos)
+int s6dns_message_parse_question (s6dns_message_counts_t *counts, s6dns_domain_t *name, uint16_t *qtype, char const *packet, unsigned int packetlen, unsigned int *pos)
 {
   uint16_t qclass ;
+  if (!counts->qd) return (errno = EINVAL, 0) ;
   if (!s6dns_message_get_domain(name, packet, packetlen, pos)) return 0 ;
   if (*pos + 4 > packetlen) return (errno = EPROTO, 0) ;
   uint16_unpack_big(packet + *pos, qtype) ; *pos += 2 ;
   uint16_unpack_big(packet + *pos, &qclass) ; *pos += 2 ;
   if (qclass != S6DNS_C_IN) return (errno = ENOTSUP, 0) ;
+  counts->qd-- ;
   return 1 ;
 }
