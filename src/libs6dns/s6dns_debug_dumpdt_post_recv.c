ea9f0d8f2853b512e841ef2d6ca2afa3792616b8
Laurent Bercot <ska-skaware@skarnet.org>
2024-06-17 20:20:09 +0000

    Add packet dump in post-received debug primitive
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libs6dns/s6dns_debug_dumpdt_post_recv.c
+++ src/libs6dns/s6dns_debug_dumpdt_post_recv.c
@@ -1,13 +1,16 @@
 /* ISC license */
 
 #include <skalibs/genwrite.h>
+
+#include <s6-dns/s6dns-analyze.h>
 #include <s6-dns/s6dns-debug.h>
 
 int s6dns_debug_dumpdt_post_recv (s6dns_engine_t const *dt, void *data)
 {
   genwrite *gp = data ;
   (void)dt ;
-  if ((*gp->put)(gp->target, "Received a packet\n", 19) < 19) return 0 ;
+  if ((*gp->put)(gp->target, "Received a packet:\n", 19) < 19) return 0 ;
+  if (!s6dns_analyze_packet(gp, dt->sa.s + dt->querylen, dt->sa.len - dt->querylen, 1)) return 0 ;
   if ((*gp->put)(gp->target, "\n", 1) < 1) return 0 ;
   return (*gp->flush)(gp->target) ;
 }
